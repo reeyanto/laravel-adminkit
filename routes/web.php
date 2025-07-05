d3656ea791b55b4abaf2203379ed90640ff951b6
Riyanto <riyanto@Riyantos-MacBook-Pro.local>
2025-07-05 23:49:29 +0700

    Menambahkan form signin dan signup

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('auth.signin');
+})->name('auth.signin');
+
+Route::get('/signup', function() {
+    return view('auth.signup');
+})->name('auth.signup');
+
+Route::get('/dashboard', function() {
     return view('admin.blank.index');
-});
+})->name('admin.dashboard');
